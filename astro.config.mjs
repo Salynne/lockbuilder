41d33ccfaab1f61a46b404bc36c1b4ce8d876640
Selene Xenia <selene.thea.xenia@gmail.com>
2026-05-09 09:41:41 -0700

    Added basic server deploy setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,21 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.firelockbuilder.io",
+  output: "server",
+
+  server: {
+    host: '0.0.0.0'
+  },
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: node({
+    mode: 'standalone'
+  })
 });
